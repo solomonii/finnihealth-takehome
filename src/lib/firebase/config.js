23b24d878ebdf43deb576a5a2c51f5db3f7eb113
solomonii <lee09@college.harvard.edu>
2023-12-01 16:10:05 -0500

    set up firebase auth, firestore

---
+++ src/lib/firebase/config.js
@@ -0,0 +1,7 @@
+export default {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+};
